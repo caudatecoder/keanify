05c5b492e75cdfc1bd523dc3301f969ea7c0cf2b
Alexey Suslyakov <alexey.suslyakov@sparksolutions.co>
2019-07-10 23:08:33 +0200

    Add chrome compatibility

--- src/background.js
+++ src/background.js
@@ -1,12 +1,25 @@
-browser.contextMenus.create({
+const menuItem = {
     id: "keanify",
-    title: "Keanify"
-});
-  
-browser.contextMenus.onClicked.addListener(function(info) {
-    if (info.menuItemId == "keanify") {
-        browser.tabs.executeScript({
-            file: "src/main.js"
-        });
+    title: "Keanify",
+    type: 'normal'
+}
+
+function clickHandler(userAgent) {
+    return function(info) {
+        if (info.menuItemId == "keanify") {
+            userAgent.tabs.executeScript({
+                file: "src/main.js"
+            });
+        }
     }
-});
+}
+
+try {
+    browser.contextMenus.create(menuItem); // Throws reference error in chrome
+    browser.contextMenus.onClicked.addListener(clickHandler(browser));
+} catch (error) {
+    chrome.runtime.onInstalled.addListener(function() {
+        chrome.contextMenus.create(menuItem);
+    })
+    chrome.contextMenus.onClicked.addListener(clickHandler(chrome))
+}
