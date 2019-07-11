42dfb45d2f8f1cdbe7bd765affc6e0acebe11cb0
Alexey Suslyakov <alexey.suslyakov@sparksolutions.co>
2019-07-11 21:13:40 +0200

    Add browser action handler & icons

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ const menuItem = {
     type: 'normal'
 }
 
-function clickHandler(userAgent) {
+function contextMenuHandler(userAgent) {
     return function(info) {
         if (info.menuItemId == "keanify") {
             userAgent.tabs.executeScript({
@@ -14,12 +14,22 @@ function clickHandler(userAgent) {
     }
 }
 
+function browserActionHandler(userAgent) {
+    return function() {
+        userAgent.tabs.executeScript({
+            file: "src/main.js"
+        });
+    }
+}
+
 try {
     browser.contextMenus.create(menuItem); // Throws reference error in chrome
-    browser.contextMenus.onClicked.addListener(clickHandler(browser));
+    browser.contextMenus.onClicked.addListener(contextMenuHandler(browser));
+    browser.browserAction.onClicked.addListener(browserActionHandler(browser))
 } catch (error) {
     chrome.runtime.onInstalled.addListener(function() {
         chrome.contextMenus.create(menuItem);
     })
-    chrome.contextMenus.onClicked.addListener(clickHandler(chrome))
+    chrome.contextMenus.onClicked.addListener(contextMenuHandler(chrome))
+    chrome.browserAction.onClicked.addListener(browserActionHandler(chrome))
 }
